4332a5b51e64ba41922b3e868b356cf531057820
Karpovich Yuliya <375339008080.yuluya@gmail.com>
2026-01-07 13:30:56 +0300

    Docker

--- src/test/java/org/example/library4/Library4ApplicationTests.java
+++ src/test/java/org/example/library4/Library4ApplicationTests.java
@@ -1,9 +1,9 @@
 package org.example.library4;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class Library4ApplicationTests {
 
     @Test
